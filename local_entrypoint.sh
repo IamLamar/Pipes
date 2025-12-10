5449932b78cf7ab947dc9d46f0f338bdcaf1617a
Arseniy <arseniydruzhinin985@gmail.com>
2025-12-10 16:15:03 +0300

    добавил логирования в ентрипоинт

--- local_entrypoint.sh
+++ local_entrypoint.sh
@@ -8,6 +8,22 @@ done
 
 echo "PostgreSQL started!"
 
+# Выводим ключевые настройки Django перед миграциями
+python manage.py shell -c "
+from django.conf import settings
+from django.apps import apps
+from pprint import pprint
+
+print('DJANGO_SETTINGS_MODULE =', settings.SETTINGS_MODULE)
+print('PROD =', getattr(settings, 'PROD', None))
+db = settings.DATABASES.get('default', {}).copy()
+db.pop('PASSWORD', None)
+print('DATABASE CONFIG (without password):')
+pprint(db)
+for app_label in ['apps.mainpage', 'apps.service_page', 'apps.products', 'apps.projects', 'apps.contacts', 'apps.company']:
+    print(f'{app_label} installed =', apps.is_installed(app_label))
+" || true
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
